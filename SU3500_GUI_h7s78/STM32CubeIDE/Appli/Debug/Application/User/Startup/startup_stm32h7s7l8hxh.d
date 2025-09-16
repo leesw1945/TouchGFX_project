4a4ded04dfd10e2fc709e23115ac6158db230700
sewon <leesw1945@gmail.com>
2025-09-16 19:07:16 +0900

    USB to TTL 테스트

--- SU3500_GUI_h7s78/STM32CubeIDE/Appli/Debug/Application/User/Startup/startup_stm32h7s7l8hxh.d
+++
@@ -1,2 +0,0 @@
-Application/User/Startup/startup_stm32h7s7l8hxh.o: \
- ../Application/User/Startup/startup_stm32h7s7l8hxh.s
---
+++ SU6000_Center/Debug/sources.mk
@@ -0,0 +1,51 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+C++M_SRCS := 
+CPP_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+ELF_SRCS := 
+C_UPPER_SRCS := 
+CXX_SRCS := 
+CCM_SRCS := 
+C++_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+CC_SRCS := 
+C_SRCS := 
+CXXM_SRCS := 
+CYCLO_FILES := 
+OBJDUMP_LIST := 
+CCM_DEPS := 
+C_UPPER_DEPS := 
+S_DEPS := 
+CXXM_DEPS := 
+C_DEPS := 
+CC_DEPS := 
+SIZE_OUTPUT := 
+C++_DEPS := 
+SU_FILES := 
+EXECUTABLES := 
+OBJS := 
+CXX_DEPS := 
+MAP_FILES := 
+S_UPPER_DEPS := 
+C++M_DEPS := 
+CPP_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+Core/Src \
+Core/Startup \
+Drivers/STM32U5xx_HAL_Driver/Src \
+Middlewares/ST/usbx/common/core/src \
+Middlewares/ST/usbx/common/usbx_device_classes/src \
+Middlewares/ST/usbx/common/usbx_stm32_device_controllers \
+TouchGFX/App \
+TouchGFX/target \
+TouchGFX/target/generated \
+USBX/App \
+
